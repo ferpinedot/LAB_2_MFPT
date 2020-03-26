11b91f0d4c8dfac4135eb0166f5e4c2cb0bb20e1
ferpinedot <47230287+ferpinedot@users.noreply.github.com>
2020-03-26 17:34:32 -0600

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  26 17:32:59 2020
+
+@author: User
+"""
---
+++ visualizaciones.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  26 17:34:22 2020
+
+@author: User
+"""
