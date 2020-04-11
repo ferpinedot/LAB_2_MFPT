37f6e6f27aa138af23c4bbc6d82a9b46db86e8d2
ferpinedot <47230287+ferpinedot@users.noreply.github.com>
2020-04-11 17:30:53 -0500

    Last changes (plotly)

--- visualizaciones.py
+++ visualizaciones.py
@@ -8,9 +8,9 @@ Created on Wed Apr  8 17:37:44 2020
 import matplotlib.pyplot as plt
 import funciones as fn
 import plotly.graph_objects as go
-import plotly as py
-py.offline.init_notebook_mode(connected = True)
-from plotly.offline import plot
+import plotly.offline as py
+py.offline.init_notebook_mode(connected = False)
+#from plotly.offline import iplot
 import pandas as pd
  
 
@@ -59,28 +59,29 @@ def profit_acum(profit_d):
     """
     profit_d = fn.f_profit_diario(datos)
     fig = go.Figure(data = [go.Bar(x = profit_d['timestamp'], y = profit_d['profit_acm_d'])], layout_title_text = "A fig.show()")
-    fig.show()
+    # fig.show()
+    py.iplot(fig)
     
 
+# def profit_acum_line(profit_d):
+#     """
+#     Parameters
+#     ----------
+#     profit_d : función : Función utilizada para el dataframe con el pd.DataFrame(datos)
 
-def profit_acum_line(profit_d):
-    """
-    Parameters
-    ----------
-    profit_d : función : Función utilizada para el dataframe con el pd.DataFrame(datos)
+#     Returns
+#     -------
+#     graph : gráfica de línea con plotly mostrando el profit acumulado
 
-    Returns
-    -------
-    graph : gráfica de línea con plotly mostrando el profit acumulado
-
-    """
-    profit_d = fn.f_profit_diario(datos)
-    profs = go.Figure(data = [go.Scatter(x= profit_d['timestamp'], y = profit_d['profit_acm_d'])])
-    profs.update_layout(title = "Profit acumulado al día", xaxis_title = "Tiempo (fechas)", yaxis_title = "Profit ($)")
-    
-    profs.show()
+#     """
+#     profit_d = fn.f_profit_diario(datos)
+#     profs = go.Figure(data = [go.Scatter(x= profit_d['timestamp'], y = profit_d['profit_acm_d'], mode = 'lines', marker = dict(color = 'Black'))])
+#     profs.update_layout(title = "Profit acumulado al día", xaxis_title = "Tiempo (fechas)", yaxis_title = "Profit ($)")
+#     #profs.show()
+#     py.iplot(profs)
 
 
+# Gráfica de ranking
 def ranking(estadisticas):
     """
     Parameters
@@ -92,32 +93,80 @@ def ranking(estadisticas):
     graph : gráfica de pastel con plotly mostrando el porcentaje que representa la asertividad del total de pares usados
 
     """
-    estadisticas = fn.f_stats_mad(datos)
+    estadisticas = fn.f_basic_stats(datos)
     df_ranking = pd.DataFrame(estadisticas['df_1_ranking'])
     df_1_ranking = df_ranking.reset_index()
     df_ranking = df_1_ranking.rename(columns = {"index": "pares", "rank": "rank"})
     
+    pie_rank = go.Figure()
     labels = df_ranking['pares']
     values = df_ranking['rank']
-    pie_rank = go.Figure(data = [go.Pie(labels=labels, values=values, pull=[0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], title = 'Asertividad de pares')])
+    pie_rank = go.Figure(data = [go.Pie(labels=labels, values=values, pull=[0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])])
     pie_rank.update_layout(title = "Ranking de asertividad de pares", font = dict(size = 16))
-    pie_rank.show()
+    #pie_rank.show()
+    py.iplot(pie_rank)
+    
 
-def sesgos_graph(sesgos):
+profit_d = fn.f_profit_diario(datos)
+
+# Gráfica drawdown y drawup
+def profit_dd_du_line(profit_d):
     """
     Parameters
     ----------
-    sesgos : función : Función utilizada para calcular los sesgos cognitivos 
+    profit_d : función : Función utilizada para el dataframe con el pd.DataFrame(datos)
 
     Returns
     -------
-    graph : gráfica de barras representando valores porcentuales del status-quo y aversión al riesgo
+    graph : gráfica de línea con plotly mostrando el profit acumulado
 
     """
+    profit_d = fn.f_profit_diario(datos)
+
+    profs = go.Figure()
+    profs.add_trace(go.Scatter(x = profit_d.timestamp, y = [None, None, None, None, 4529.32, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 4989.32, None, None, None, None, None, None, None, None, None, None, None, None] , name = 'drawup', connectgaps=True, mode = 'lines', line={'dash': 'dash', 'color': 'green'}))
+    profs.add_trace(go.Scatter(x = profit_d.timestamp, y = [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 4989.32, None, None, None, None, None, None, None, 4117.03, None, None, None, None] , name = 'drawdown', connectgaps=True, mode = 'lines', line={'dash': 'dash', 'color': 'red'}))
+    profs.add_trace(go.Scatter(x= profit_d.timestamp, y = profit_d.profit_acm_d, name = 'profit acumulado', mode = 'lines', marker = dict(color = 'Black')))
     
-    sesgos = fn.f_sesgos_cognitivos(datos)
-    df_resultados = pd.DataFrame(sesgos['resultados'])
-    df_results = df_resultados.drop([0, 3])
-    sesgs_bar = go.Figure(data = [go.Bar(x = df_results['mediciones'], y = df_results['resultados'])])
-    sesgs_bar.update_layout(title = "Disposition Effect", xaxis_title = "Mediciones", yaxis_title = "Resultados (%)")    
-    sesgs_bar.show()
+    profs.update_layout(title = "Profit acumulado al día", xaxis_title = "Tiempo (fechas)", yaxis_title = "Profit ($)")
+    
+    #profs.show()
+    py.iplot(profs)
+
+
+
+# def sesgos_graph(sesgos):
+#     """
+#     Parameters
+#     ----------
+#     sesgos : función : Función utilizada para calcular los sesgos cognitivos 
+
+#     Returns
+#     -------
+#     graph : gráfica de barras representando valores porcentuales del status-quo y aversión al riesgo
+
+#     """
+    
+#     sesgos = fn.f_sesgos_cognitivos(datos)
+#     df_resultados = pd.DataFrame(sesgos['resultados'])
+#     df_results = df_resultados.drop([0, 3])
+#     sesgs_bar = go.Figure(data = [go.Bar(x = df_results['mediciones'], y = df_results['resultados'])])
+#     sesgs_bar.update_layout(title = "Disposition Effect", xaxis_title = "Mediciones", yaxis_title = "Resultados (%)")    
+#     sesgs_bar.show()
+ 
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
